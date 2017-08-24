abde08ca4c5a2c5609901ae5d1df6517a78394f7
Alexander Svistunov <alexsvistunov73@gmail.com>
2017-08-24 12:47:53 +0300

    add posts for blog

---
+++ src/posts.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    title: 'Sit modi quod assumenda non blanditiis sint',
+    body: 'Ipsum sequi est officia nobis. Excepturi sit earum amet ea et. Et ea laboriosam placeat asperiores cupiditate asperiores ea suscipit. Est qui officia quaerat earum. Adipisci rem voluptatem quia totam perferendis illo laudantium mollitia. Corporis quisquam dignissimos a alias eum. Distinctio quisquam corporis veritatis dolorem consequatur consequuntur et id. Autem vitae qui eum voluptas. Error omnis nulla omnis quos asperiores eum.'
+  },
+  {
+    title: 'Et dignissimos id temporibus nemo consectetur quis',
+    body: 'Nemo eum odit velit recusandae accusantium dicta Qui minima ipsam qui ut Eos molestiae itaque ipsam a sint ratione Vitae est hic iure ut deserunt Numquam in quia et dolorem Alias doloribus cumque esse asperiores. Hic odio non aut maxime. Nobis ipsum at voluptas aut Enim soluta at tempore Consectetur quis commodi commodi minus sit.'
+  },
+  {
+    title: 'Fuga molestiae nesciunt dolorem quia',
+    body: 'Nobis sint minus molestiae. iste et fuga accusamus Tenetur deserunt delectus ipsum qui consequatur dolor Ratione corporis minus quo placeat Qui ipsam ipsam voluptatibus aliquid odit id. Deleniti aut fugiat suscipit. Nostrum inventore quae est eos voluptatem. Tenetur ipsum et pariatur tempore Iusto labore at molestias Natus quis at dolorem.'
+  },
+  {
+    title: 'Quam et sit omnis modi',
+    body: 'Sit et corrupti dignissimos molestiae non nostrum vero. Sit quidem excepturi qui inventore blanditiis et amet. Odio qui aut tempore qui dolore quibusdam Cupiditate dolores pariatur vel nobis sed dolor ipsum. Hic et sapiente ut. dolor voluptates. Commodi et odio nam excepturi est enim. Nulla amet similique ea ducimus quidem Cumque dolores est dolor eaque.'
+  },
+  {
+    title: 'Iste dolor est soluta',
+    body: 'Assumenda qui dolorem quia sit hic ea qui. Dolorum esse ut sunt ut eveniet quis dignissimos. Cumque nihil quae ut Temporibus debitis et est Tenetur eos Nihil eos voluptas aliquam amet pariatur. impedit nisi Beatae nesciunt omnis qui velit veniam aut. Est illo soluta ratione. et mollitia. Voluptas vero voluptatum reiciendis aut est.'
+  }
+]
